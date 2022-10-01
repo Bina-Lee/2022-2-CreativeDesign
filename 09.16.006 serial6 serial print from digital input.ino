6b903a189563fd8576148b88fb97acbe617fca94
이진성 <105117441+Bina-Lee@users.noreply.github.com>
2022-10-02 06:45:18 +0900

    Create 09.16.006 serial6 serial print from digital input.ino

---
+++ 09.16.006 serial6 serial print from digital input.ino
@@ -0,0 +1,32 @@
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int ledPin =  13;      // the number of the LED pin
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT_PULLUP);
+  //PULLUP을 하면 별도 저항구성 없이 저항을 연결해서 5V를 연결한 효과를 줌
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+    Serial.println("눌림");
+    while(1){
+      if(buttonState==HIGH)break;
+    }
+  }
+}
