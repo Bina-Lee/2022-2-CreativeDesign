aa4b0ffa2729b8f84c86d28d274ecd9171b0dfb7
이진성 <105117441+Bina-Lee@users.noreply.github.com>
2022-10-12 10:52:54 +0900

    Create sketch3.js

---
+++ 009 i2c lcd serial with p5/sketch3.js
@@ -0,0 +1,48 @@
+let lcdFont, input0, input1;
+let serial;
+
+function preload() {
+  lcdFont = loadFont('assets/5x8_lcd.ttf');
+}
+ 
+function setup() {
+  createCanvas(450, 240);
+  background(100,150,255);
+  noStroke();
+  fill('#FFFFFF');
+  textFont(lcdFont);
+  textSize(36);
+  text('Hello, world!',   10,  50);
+  text('Arduino at INU!', 10, 100);
+  input0 = createInput('Hello, world!');
+  input0.position(5, 120 );
+  input0.size(420);
+  input0.input(input0Event);
+  input1 = createInput('Arduino at INU!');
+  input1.position(5, 180 );
+  input1.size(420);
+  input1.input(input1Event);
+
+  serial = new p5.SerialPort();
+  serial.open("COM3");
+  serial.write("0Hello, world!\r\n");
+  serial.write("1Arduino at INU!\r\n");
+}
+
+function input0Event() {
+  console.log('input0: ', this.value());
+  fill(100,150,255);
+  rect(0,0, 450, 60);
+  fill('#FFFFFF');
+  text(this.value(),   10,  50);
+  serial.write("0" + this.value() + "\r\n");
+}
+
+function input1Event() {
+  console.log('input1: ', this.value());
+  fill(100,150,255);
+  rect(0,60, 450, 60);
+  fill('#FFFFFF');
+  text(this.value(),   10, 100);
+  serial.write("1" + this.value() + "\r\n");
+}
